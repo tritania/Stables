9a38cafca893abeee9f47d123318e4232d00c27e
Tritania <erikwilson@magnorum.com>
2014-05-12 19:36:08 -0300

    Finished teleport to class.

--- src/main/java/org/tritania/horseteleport/command/Teleport.java
+++ src/main/java/org/tritania/horseteleport/command/Teleport.java
@@ -28,9 +28,16 @@ import org.bukkit.entity.Horse;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Vehicle;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.InventoryHolder;
+import org.bukkit.Material;
 
 import org.tritania.horseteleport.HorseTeleport;
 import org.tritania.horseteleport.util.Message;
+
+import static org.bukkit.entity.Horse.*;
 /*End Imports*/
 
 public class Teleport implements CommandExecutor 
@@ -48,13 +55,43 @@ public class Teleport implements CommandExecutor
 		if (player.hasPermission("horseteleport.teleport"))
 		{
 			Player playertwo = Bukkit.getPlayer(args[2]);
-			Entity vech = playertwo.getVehicle();
-			Vehicle vec = (Vehicle)vech;
-			//teleport
+			Entity vech = player.getVehicle();
+			
+			Location ptl = playertwo.getLocation();
+			World ptw = playertwo.getWorld();
+			player.teleport(ptl);
+			
+			Horse en = (Horse)vech;
+			Variant vart = en.getVariant();
+			Color col = en.getColor();
+			Style sty = en.getStyle();
+			int dom = en.getDomestication();
+			int life = en.getTicksLived();
+			ItemStack arm = en.getInventory().getArmor();
+			double ju = en.getJumpStrength();
+			double health = en.getMaxHealth();
+			
+			en.remove();
+			
+			en = ptw.spawn(ptl, Horse.class);
+			en.setColor(col);
+			en.setDomestication(dom);
+			en.setVariant(vart);
+			en.setStyle(sty);
+			en.setTicksLived(life);
+			en.setTamed(true);
+			en.getInventory().setSaddle(new ItemStack(Material.SADDLE, 1));
+			en.getInventory().setArmor(arm);
+			en.setJumpStrength(ju);
+			en.setMaxHealth(health);
+			
+			((Horse)en).setPassenger(player);		
 		}
 		else
 		{
-			//just teleport player
+			Player playertwo = Bukkit.getPlayer(args[2]);
+			Location ptl = playertwo.getLocation();
+			player.teleport(ptl);
 		}
 		return true;
 	}
